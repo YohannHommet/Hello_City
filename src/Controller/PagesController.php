353b957c97011880962eaa863c0d35d39eeecea3
Yohann <YohannHommet@users.noreply.github.com>
2020-10-06 18:59:56 +0200

    Init Hello-City, Add twig and maker bundle, php.ini config, setup PagesController and homepage

---
+++ src/Controller/PagesController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class PagesController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_home")
+     * 
+     * @return Response
+     */    
+
+    public function home(): Response
+    {
+        return $this->render('pages/home.html.twig');
+    }
+}
